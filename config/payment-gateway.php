d4104e366efe19d72f8e99b46b0a0193acb997ce
Wayne Lai <waynelai06@gmail.com>
2024-08-09 16:33:16 +0800

    fix: payment url

--- config/payment-gateway.php
+++ config/payment-gateway.php
@@ -3,12 +3,14 @@
 return [
     'staging' => [
         'paymentUrl' => 'https://ttpayment-gateway.currenttech.pro',
-        'appId' => 'ttpay',
+        'appId' => 'qcg',
+        'ttKey' => 'e74kEN9rKTsMi3sPfqNnx7wrpzYk2WLSd0mwmOO8',
         'merchantId' => '13'
     ],
     'live' => [
         'paymentUrl' => 'https://securepay.ttpays.io',
-        'appId' => 'ttpay',
+        'appId' => 'qcg',
+        'ttKey' => 'DWP1aKJHDZu98dXJueMA5r2d6x0K9urfpXf6WehNzIVPOw0289',
         'merchantId' => '2'
     ],
 ];
