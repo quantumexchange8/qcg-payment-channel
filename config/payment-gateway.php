12972f5874c8225fe38126a21aa6c5d2a62d4a4a
Wayne Lai <waynelai06@gmail.com>
2024-10-29 14:08:25 +0800

    fix: change payment url

--- config/payment-gateway.php
+++ config/payment-gateway.php
@@ -8,7 +8,7 @@ return [
         'merchantId' => '13'
     ],
     'live' => [
-        'paymentUrl' => 'https://securepay.ttpays.io',
+        'paymentUrl' => 'https://securepay.ttbvnk.com',
         'appId' => 'qcg',
         'ttKey' => 'DWP1aKJHDZu98dXJueMA5r2d6x0K9urfpXf6WehNzIVPOw0289',
         'merchantId' => '2'
